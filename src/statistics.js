23e19f052d96a7d0252d0ed178ac19d09c308192
= <=>
2020-02-13 16:25:47 +0100

    Add prettier

--- src/statistics.js
+++ src/statistics.js
@@ -24,10 +24,20 @@ type StatisticsObjType = {
 export const updateStatistics = (statistics: StatisticsObjType) => {
     let statisticsHtml = `
         <span class="statistics__item">Cell spaces: ${statistics.totalCells}</span>
-        <span class="statistics__item">Living: ${statistics.livingCells} (${Math.floor((statistics.livingCells / statistics.totalCells) * 100)}%)</span>
+        <span class="statistics__item">Living: ${statistics.livingCells} (${Math.floor(
+        (statistics.livingCells / statistics.totalCells) * 100
+    )}%)</span>
         <span class="statistics__item">Generation: ${statistics.generation}</span>
-        <span class="statistics__item">Paint speed: ${getAvgTime(paintTimes, paintSpeedsToRemember, statistics.paintTime)} ms</span>
-        <span class="statistics__item">Calculation speed: ${getAvgTime(calcTimes, calcSpeedsToRemember, statistics.calcTime)} ms</span>
+        <span class="statistics__item">Paint speed: ${getAvgTime(
+            paintTimes,
+            paintSpeedsToRemember,
+            statistics.paintTime
+        )} ms</span>
+        <span class="statistics__item">Calculation speed: ${getAvgTime(
+            calcTimes,
+            calcSpeedsToRemember,
+            statistics.calcTime
+        )} ms</span>
     `;
 
     if (statisticsElement) {
